c1291af3fdd9daae4aa3937ff9fa1a7d6e2f753c
Ayelen <ayelenreinglestein@gmail.com>
2026-02-21 02:02:38 -0300

    feat: add ProductSkeleton for better loading experience

--- src/pages/shop/containers/ContainerProducts/ContainerProducts.jsx
+++ src/pages/shop/containers/ContainerProducts/ContainerProducts.jsx
@@ -8,10 +8,6 @@ import { ProductSkeleton } from "../../components/ProductCard/ProductSkeleton"
 export const ContainerProducts = () => {
     const { products, loading } = useProducts()
 
-    if (loading && products.length === 0) {
-        return <ProductSkeleton />
-    }
-
     if (!Array.isArray(products)) {
     return <p>Error cargando productos</p>
     }
@@ -19,7 +15,11 @@ export const ContainerProducts = () => {
     return(
         <>
         <div className={styles.containerProducts}>
-            { products.length === 0 ? (
+            { loading ? (
+                Array.from({ length: 8 }).map((_, index) => (
+                    <ProductSkeleton key={index} />
+                ))
+            ) : products.length === 0 ? (
                 <div className={styles.containerNoResults}>
                     <img src={imgResults} alt="No hay resultados" className={styles.imgNoResults}/>
                     <p className={styles.titleNoResults}>Lo sentimos, no encontramos lo que buscas</p>
